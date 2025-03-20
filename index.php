8d8871940c757f163d4ffe1be53179f3e70b9643
Rifki Abdillah <rifkiabdillah68@gmail.com>
2025-03-20 22:44:29 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore fugit, quam sit blanditiis ducimus voluptatum. Repellendus numquam beatae, accusamus officia, asperiores ab omnis veniam nobis aut assumenda, quia a enim.</p>
 </body>
 </html>
